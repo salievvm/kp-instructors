dd119206dff0bd9be435420cd7cc0ead386e6eba
salievvm <salievvm@gmail.com>
2023-11-24 00:52:26 +0600

    CustomList from schema map

--- src/components/@ui/CustomList/CustomList.jsx
+++ src/components/@ui/CustomList/CustomList.jsx
@@ -19,15 +19,15 @@ import Chip from '@mui/material/Chip';
 
 const ListItemLevel = ({ level }) => {
   if (level) {
-    const length = new Array(level).fill(0);
+    const length = new Array(level).fill(".");
     return (
       <Grid
         container
         gap={2}
         justifyContent="center"
       >
-        {length.map((item) => (
-          <Typography variant="body2" sx={{ lineHeight: '14px' }}>.</Typography>
+        {length.map((separator, i) => (
+          <Typography key={i} variant="body2" sx={{ lineHeight: '14px' }}>{separator}</Typography>
         ))}
       </Grid>
     )
@@ -36,14 +36,23 @@ const ListItemLevel = ({ level }) => {
   return null;
 }
 
+ListItemLevel.propTypes = {
+  level: PropTypes.number,
+};
+
+ListItemLevel.defaultProps = {
+  level: null,
+};
+
 const ListItem = ({
   title,
   chip,
   level,
   onClick,
   open,
+  id,
 }) => {
-  console.log({ open });
+  // console.log({ id });
   return (
     <ListItemButton onClick={onClick}>
       <ListItemText primary={
@@ -77,19 +86,73 @@ const ListItem = ({
 };
 
 ListItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  chip: PropTypes.any,
+  level: PropTypes.number,
+  onClick: PropTypes.func,
+  id: PropTypes.any,
   open: PropTypes.bool,
 };
 
 ListItem.defaultProps = {
+  chip: null,
+  level: null,
+  onClick: () => { },
   open: false,
 };
 
-export default function CustomList() {
+const schema = {
+  skis: {
+    title: 'Горные лыжи и сноуборд',
+    id: 'skis',
+    items: [
+      {
+        id: 'standart',
+        title: 'Стандартные тарифы',
+        quantity: 12,
+        items: [
+          {
+            title: 'Индивидуальное занятие',
+            id: 'individual',
+            items: [
+              {
+                id: '1hour',
+                title: '1 час',
+              },
+              {
+                id: '2hours',
+                title: '2 часа',
+              },
+            ],
+          },
+          {
+            title: 'Два человека/Парное занятие',
+            id: 'twoperson',
+            items: [
+              {
+                id: '1hour',
+                title: '1 час',
+              },
+              {
+                id: '2hours',
+                title: '2 часа',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+};
+
+export default function CustomList({ }) {
   const [open, setOpen] = React.useState(true);
 
   const handleClick = () => {
     setOpen(!open);
   };
+
+  const { skis } = schema;
 
   return (
     <List
@@ -97,22 +160,33 @@ export default function CustomList() {
       component="nav"
       aria-labelledby="nested-list-subheader"
     >
-      <ListItem
-        chip={12}
-        onClick={handleClick}
-        title={`Стандартные тарифы`}
-        open={open}
-      />
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItem title={`Индивидуальное занятие`} level={1} />
-          <ListItem title={`1 час`} level={2} />
-          <ListItem title={`2 часа`} level={2} />
-          <ListItem title={`Два человека/Парное занятие`} level={1} />
-          <ListItem title={`1 час`} level={2} />
-          <ListItem title={`2 часа`} level={2} />
-        </List>
-      </Collapse>
+      {skis.items.map((item) => (
+        <React.Fragment key={item.id}>
+          <ListItem
+            chip={12}
+            onClick={handleClick}
+            title={item.title}
+            open={open}
+            id={item.id}
+          />
+          {item.items && item.items.length ? (
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                {item.items.map((subitem) => (
+                  <React.Fragment key={subitem.id}>
+                    <ListItem title={subitem.title} level={1} id={subitem.id} />
+                    {subitem.items && subitem.items.length ? (
+                      subitem.items.map((subsubitem) => (
+                        <ListItem title={subsubitem.title} level={2} id={subsubitem.id} key={subsubitem.id}/>
+                      ))
+                    ) : null}
+                  </React.Fragment>
+                ))}
+              </List>
+            </Collapse>
+          ) : null}
+        </React.Fragment>
+      ))}
     </List>
   );
 }
